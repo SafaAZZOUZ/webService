f10c41035bd5885a9a260266dcb189452a4b57c9
Safaa <s.azzouz@etu.enset-media.ac.ma>
2023-10-23 22:02:23 +0100

    v1

---
+++ src/main/java/ServeurJWS.java
@@ -0,0 +1,9 @@
+import WS.BanqueService;
+import jakarta.xml.ws.Endpoint;
+
+public class ServeurJWS {
+    public static void main(String[] args) {
+        Endpoint.publish("http://0.0.0.0:9191/",new BanqueService());
+        System.out.println("web service déployé sur http://0.0.0.0:9191/ ");
+    }
+}
